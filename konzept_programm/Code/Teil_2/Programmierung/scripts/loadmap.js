31a3e5b7f1fb3294191be813c3a0b67f79249e52
Rathijikj <felix.werkstetter@googlemail.com>
2014-05-09 11:41:19 +0200

    Update loadmap.js
    .name statt id wieder hingemacht

--- konzept_programm/Code/Teil_2/Programmierung/scripts/loadmap.js
+++ konzept_programm/Code/Teil_2/Programmierung/scripts/loadmap.js
@@ -65,7 +65,7 @@ function displayPoints(points) {
             position: markerPos,
             animation: google.maps.Animation.DROP,
             icon: iconPath,
-            title: points[i].id
+            title: points[i].name
         });
 
         google.maps.event.addListener(marker, 'click', function() {
